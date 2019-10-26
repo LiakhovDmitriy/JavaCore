87ee793b361132ef8cd86e15a4788cc58f5d7b5f
Dmitriy <dimaliahov@gmail.com>
2019-10-26 15:01:28 +0300

    qwe

---
+++ Lesson2Ex1Hw1/src/Board.java
@@ -0,0 +1,46 @@
+import java.text.DecimalFormat;
+import java.util.Arrays;
+
+public class Board {
+    private Shape[] Board = new Shape[4];
+
+    public Board() {
+        super();
+    }
+
+    public void added(Shape name, int number) {
+        if (Board[number - 1] == null && number - 1 < Board.length && number - 1 >= 0) {
+            Board[number - 1] = name;
+        } else System.out.println("This position is used");
+    }
+
+    public void kill(int number) {
+        if (Board[number - 1] != null && number - 1 < Board.length && number - 1 >= 0) {
+            Board[number - 1] = null;
+        } else System.out.println("This position already null");
+    }
+
+    public double boardSqr() {
+        double sqrt = 0;
+        for (Shape shape : Board) {
+            if (shape != null) {
+                sqrt += shape.getArea();
+            }
+        }
+        return sqrt;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < Board.length; i++) {
+            if (Board[i] != null) {
+                sb.append(((i + 1) + ") " + Board[i]));
+            } else sb.append((i + 1) + ") " + "It's empty");
+            sb.append(System.lineSeparator());
+        }
+        DecimalFormat f = new DecimalFormat("##.00");
+        sb.append("Total area is " + f.format(boardSqr()));
+        return sb.toString();
+    }
+}
--- Lesson2Ex1Hw1/src/Main.java
+++ Lesson2Ex1Hw1/src/Main.java
@@ -7,6 +7,9 @@ public class Main {
      * Создайте класс Point, в котором есть два свойства double x double y.
      * Создайте классы, которые описывают, как минимум, три геометрические фигуры (они должны быть подклассами
      * Shape). При этом они в качестве свойств должны содержать классы Point.
+     * Создайте класс доска. Доска поделена на 4 части в каждую часть доски можно положить одну из фигур. У доски должны
+     * быть методы которые помещают и удаляют фигуру с доски.
+     * Также должен быть метод который выводит информацию о всех фигурах лежащих на доске и их суммарную площадь.
      */
 
     public static void main(String[] args) {
@@ -24,11 +27,21 @@ public class Main {
 
         Circle cr = new Circle(a, b);
         System.out.println("Circle perimeter " + f.format(cr.getPerimetr()) + " centimeter");
-        System.out.println("Area of a circle " + f.format(cr.getArea()) + " square centimeter"+ "\n");
+        System.out.println("Area of a circle " + f.format(cr.getArea()) + " square centimeter" + "\n");
 
         Rectangle rec = new Rectangle(a, b, c, d);
         System.out.println("Rectangle perimeter " + f.format(rec.getPerimetr()) + " centimeter");
-        System.out.println("Area of a rectangle " + f.format(rec.getArea()) + " square centimeter");
+        System.out.println("Area of a rectangle " + f.format(rec.getArea()) + " square centimeter" + "\n");
+
+        Board bor = new Board();
+
+        bor.added(tr, 1);
+        bor.added(cr, 2);
+        bor.added(rec,4);
+
+        System.out.println(bor + "\n");
+        bor.kill(4 );
+        System.out.println(bor + "\n");
 
     }
 
